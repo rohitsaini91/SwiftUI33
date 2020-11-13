cc1010d31f2d325f2df101bec70d4a5026c57360
Rohit Saini <sketchlearn7@gmail.com>
2020-11-13 23:40:42 +0530

    Initial Commit

---
+++ Shared/SwiftUI33App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI33App.swift
+//  Shared
+//
+//  Created by Rohit Saini on 13/11/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI33App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
